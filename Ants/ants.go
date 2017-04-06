9d9ca8e5ffe4c8e0fd166df144465287397d65db
Pedro Allen Revez <pedroallenrevez@gmail.com>
2017-04-06 15:38:29 +0200

    ant code. added main with flow

--- Ants/ants.go
+++ Ants/ants.go
@@ -2,59 +2,116 @@ package Ant
 
 import (
 	"fmt"
+	"github.com/pedroallenrevez/world"
+	"math"
+	"math/rand"
+	"time"
 )
 
 //Ant badjriziz
 type Ant struct {
-	location      int
-	route         []int
-	alpha         float64
-	distTravelled float64
-	forwardMode   bool
-	//working mode
+	location      NodeID  // present location of ant
+	route         []int   // list of navigated nodes
+	alpha         float64 // experimental value - influences pheromones
+	beta          float64 // experimental value - influences desirability
+	distTravelled float64 // total distance travelled
+	tourComplete  bool    // ? ant reached goal
+	firstPass     bool    // ? first iteration
+	world         World   // * -> worldMap
 }
 
-func (ant *Ant) pickPath() {
-	//OR
-	switch ant.forwardMode {
-	case True:
-		//probability
-	case False:
-		//retrace route
-		//drop pheromone
+//initializes ants and resets them for new iteration
+func (ant *Ant) init() {
+	ant.location = 0
+	ant.route = nil
+	append(ant.route, 0)
+	ant.alpha = 1.0
+	ant.distTravelled = 0
+	tourComplete = false
+	//set world
+}
+
+// pickPath
+// randomly selects a path based on the ACO algorithm
+func (ant *Ant) pickPath() Node {
+	// termination cases
+	// if node not connected to anyone but last
+	var path int
+	var nodes = world.possibleMoves(ant.location)
+	if ant.firstPass {
+		//choose randomly even from all nodes
+		return nodes[randInt(0, len(nodes))]
 	}
+
+	interval := make([]float64)
+	accumulate = 0
+	for i, node := range nodes {
+		probability := math.Pow(node.pheromone, ant.alpha) * //priori
+			math.Pow(1.0/node.distance, ant.beta) //posterior
+		append(interval, probability)
+		accumulate += probability
+	}
+	random = randFloat() * accumulate
+	for i := range interval {
+		nodes[i] /= accumulate
+		if random < interval[i] {
+			//i dont have the id!
+			//dont need cus indexes match
+			return nodes[i]
+		}
+	}
+
 }
 
+// traverse
+// updates position on map and ant, updates route, update distance
 func (ant *Ant) traverse(start, end int) {
+	world.updatePosition(start, end)
 	ant.location = end
 	//route ++ end
 	append(ant.route, end)
 	//dist += dist callback
+	ant.distTravelled += world.calculateDistance(start, end)
 
 }
 
+//do I need this? just eliminate loops on world
 func (ant *Ant) isFood(location int) bool {
+	//reached food
 	//switch mode
+	//ant.forwardMode = false
 	//delete loops
+	if world.isFood(ant.location) {
+		//delete loops
+		/*for i := range ant.route {
+			// ? cross correct loops and distance travelled
+			//floyd loop deletion
+		}*/
+	}
 }
 
-func (ant *Ant) isNest(location int) bool {
-	//switch mode
-	//delete loops
-}
-
-func (ant *Ant) run(possMoves []int) {
+// returns a list of node ids
+func (ant *Ant) run() []int {
+	//GO ROUTINE
 	//run until tour is complete
+	for !tourComplete {
+		//next node
+		var next = pickPath()
+		traverse(ant.location, next.id)
+		if isFood(next) {
+			//tour complete
+			tourComplete = true
+		}
+	}
+	return ant.route
+}
 
-	//while possible moves
-	//pickpath
-	//traverse
-	var next = pickPath()
-	traverse(ant.location, next)
-	if isFood(next) {
-		//switch modes
-	}
-	if isNest(next) {
-		//finish tour
-	}
+func randInt(min, max int) int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(max-min) + min
+}
+
+func randFloat() float64 {
+	rand.Seed(time.Now().Unix())
+	return rand.Float64()
 }
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//init world
+	//init k ants(threads)
+	//start ant threads
+	//join threads
+	//decay map
+	//updateWorld with ant routes ( shortest dist, pheromone)
+	//ant reset
+}
